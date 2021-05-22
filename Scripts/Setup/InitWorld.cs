9529c30ad0cad80cb68f94223539bd436d3c8dcc
SergioSugaharaC <42353416+SergioSugaharaC@users.noreply.github.com>
2021-05-22 17:20:56 -0500

    Add files via upload

---
+++ Scripts/Setup/InitWorld.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class InitWorld : MonoBehaviour{
+    private Layers layersManager = new Layers();
+    GameObject go;
+    GameObject points;
+    GameObject[] allChildren;
+
+    // Start is called before the first frame update
+    void Start(){
+        layersManager.AddNewLayer("Objects");
+        layersManager.AddNewLayer("Contacts");
+        go = GameObject.Find("Chair_02");
+        go.layer = 6;
+        points = go.transform.GetChild(0).gameObject;
+        allChildren = points.GetComponentsInChildren<GameObject>();
+        
+        foreach(GameObject child in allChildren) child.layer = 7;
+    }
+}
+
+
+public class Layers {
+    private static int maxTags = 10000;
+    private static int maxLayers = 31;
+
+/////////////////////////////////////////////////////////////////////
+    public void AddNewLayer(string name){ CreateLayer(name); }
+
+    public void DeleteLayer(string name){ RemoveLayer(name); }
+////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Adds the layer.
+    /// </summary>
+    /// <returns><c>true</c>, if layer was added, <c>false</c> otherwise.</returns>
+    /// <param name="layerName">Layer name.</param>
+    public static bool CreateLayer(string layerName){
+        // Open tag manager
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        // Layers Property
+        SerializedProperty layersProp = tagManager.FindProperty("layers");
+        if (!PropertyExists(layersProp, 0, maxLayers, layerName)){
+            SerializedProperty sp;
+            // Start at layer 9th index -> 8 (zero based) => first 8 reserved for unity / greyed out
+            for (int i = 8, j = maxLayers; i < j; i++){
+                sp = layersProp.GetArrayElementAtIndex(i);
+                if (sp.stringValue == ""){
+                    // Assign string value to layer
+                    sp.stringValue = layerName;
+                    Debug.Log("Layer: " + layerName + " has been added");
+                    // Save settings
+                    tagManager.ApplyModifiedProperties();
+                    return true;
+                }
+                if (i == j) Debug.Log("All allowed layers have been filled");
+            }
+        } else Debug.Log ("Layer: " + layerName + " already exists");
+        return false;
+    }
+
+    public static string NewLayer(string name){
+        if (name != null || name != "") CreateLayer(name);
+        return name;
+    }
+
+    /// <summary>
+    /// Removes the layer.
+    /// </summary>
+    /// <returns><c>true</c>, if layer was removed, <c>false</c> otherwise.</returns>
+    /// <param name="layerName">Layer name.</param>
+    public static bool RemoveLayer(string layerName){
+        // Open tag manager
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+        // Tags Property
+        SerializedProperty layersProp = tagManager.FindProperty("layers");
+
+        if (PropertyExists(layersProp, 0, layersProp.arraySize, layerName)){
+            SerializedProperty sp;
+            for (int i = 0, j = layersProp.arraySize; i < j; i++){
+                sp = layersProp.GetArrayElementAtIndex(i);
+                if (sp.stringValue == layerName){
+                    sp.stringValue = "";
+                    Debug.Log("Layer: " + layerName + " has been removed");
+                    // Save settings
+                    tagManager.ApplyModifiedProperties();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    /// <summary>
+    /// Checks to see if layer exists.
+    /// </summary>
+    /// <returns><c>true</c>, if layer exists, <c>false</c> otherwise.</returns>
+    /// <param name="layerName">Layer name.</param>
+    public static bool LayerExists(string layerName){
+        // Open tag manager
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+        // Layers Property
+        SerializedProperty layersProp = tagManager.FindProperty("layers");
+        return PropertyExists(layersProp, 0, maxLayers, layerName);
+    }
+
+    /// <summary>
+    /// Checks if the value exists in the property.
+    /// </summary>
+    /// <returns><c>true</c>, if exists was propertyed, <c>false</c> otherwise.</returns>
+    /// <param name="property">Property.</param>
+    /// <param name="start">Start.</param>
+    /// <param name="end">End.</param>
+    /// <param name="value">Value.</param>
+    private static bool PropertyExists(SerializedProperty property, int start, int end, string value){
+        for (int i = start; i < end; i++){
+            SerializedProperty t = property.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(value)) return true;
+        }
+        return false;
+    }
+}
